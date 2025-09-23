222b3759c02826761e1067e9b1b7a462f78ae577
Michael Javan Owino Obunga <michael.obunga@strathmore.edu>
2025-09-23 21:55:07 +0300

    Create en.php
    Co-Authored-By: tashome123 <206668722+tashome123@users.noreply.github.com>

---
+++ Lang/en.php
@@ -0,0 +1,13 @@
+<?php
+$lang["ver_reg_subj"] = "Account Activation on {{site_name}}";
+
+
+$lang["ver_reg_body"] = "
+Hello {{fullname}},
+You have requested an account on <strong>{{site_name}}</strong>.
+Your activation code is:
+<h1>{{activation_code}}</h1>
+Regards,
+{{site_name}} Admin
+{{site_name}}
+";
